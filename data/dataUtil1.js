61ace624625526bc44de53526b2b3c935e5f7bc8
Crawford <David.Crawford3@jacobs.com>
2019-10-15 16:12:24 -0400

    cleaning ifs data

--- data/dataUtil1.js
+++ data/dataUtil1.js
@@ -6,7 +6,10 @@ nlu = require('./nlu.js');
 
 //returns patterns that matche 6 digit csi code and document header
 var csiPattern = /([\n][\d]{2}[\s][\d]{2}[\s][\d]{2})/gm;
-var headerPattern = /IFS Design Guide Appendix \| Rough Draft\s*\w*\s*\d*\s*\d*\s*/gm
+// var headerPattern = /IFS Design Guide Appendix \| Rough Draft\s*\w*\s*\d*\s*\d*\s*/gm
+var headerPattern = /[\w]*[\s]*[\d]*[\s]*IFS Design Guide Appendix \| Rough Draft\s*\w*\s*\d*\s*\d*\s*[\w]*[\s]*[\d]*[\s]*[\d]*/gm
+var divisionHeading = /(DIVISION[\s]*[\d]*([\s]*[A-Z]*)*[\s])/gm
+
 
 //returns array of all csi codes in ifs
 // var result = str.match(regex);
@@ -19,9 +22,12 @@ words = words.slice(1);
 
 //clean up and distribute content into datastore
 data = []
+
 for (i=0;i<words.length;i=i+2){
+    
     var text = words[i+1].slice(1).split("\n")
     var body = text.slice(1).join(" ").split(headerPattern).join("")
+    
     words[i] = words[i].slice(1); 
     data.push({
         csi: words[i],
@@ -31,9 +37,24 @@ for (i=0;i<words.length;i=i+2){
         title: text[0],
         body: body
     })
+    heading = GetHeadings(body, divisionHeading)
+    if (heading){
+        data.push(heading);
+    }
+
+
 }
 
-nlu.init(data, superagent);
+function GetHeadings (body, divisionHeading ){
+    if (divisionHeading.test(body)){
+        bodySplit = body.split(divisionHeading);
+        body = bodySplit[0]
+        var headingArr = bodySplit.slice(1)
+        return headingArr;
+    }
+}
+
+// nlu.init(data, superagent);
 
 
 
